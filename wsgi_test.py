64d95452cf61654e9f4480de09d7eec1a7388417
Andrey Shibalov <nekorro@gmail.com>
2016-05-16 21:35:06 +0300

    WSGI app for module 1.9

--- wsgi_test.py
+++ wsgi_test.py
@@ -1,3 +1,6 @@
+from wsgiref.simple_server import make_server
+import urlparse
+
 def wsgi_stepic19_app(environ, start_response):
    qs = environ['QUERY_STRING']
    output = qs.replace('&','\n')
@@ -5,4 +8,10 @@ def wsgi_stepic19_app(environ, start_response):
             ("Content-Type", "text/plain"),
             ("Content-Length", str(len(output)))
         ])
-   return iter([output])   
+   return iter([output])
+
+httpd = make_server('', 8000, wsgi_stepic19_app)
+print "Serving HTTP on port 8000..."
+
+# Respond to requests until process is killed
+httpd.handle_request()
