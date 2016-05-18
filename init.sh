0ccfc0badee74c20f449d807f10c57304c374539
Andrey Shibalov <nekorro@gmail.com>
2016-05-18 09:44:04 +0300

    Upgraded init script

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+sudo apt-get update nginx
+sudo pip install --upgrade django
+sudo pip install --upgrade gunicorn
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/ash.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
