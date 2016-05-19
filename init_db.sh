fa5f9c4fa90e475400ed8444249f34f39838dc6e
Andrey Shibalov <nekorro@gmail.com>
2016-05-19 12:30:44 +0300

    zx

--- init_db.sh
+++ init_db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # create db
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS stepic_webdev CHARACTER SET utf8;"
-mysql -u root -e "CREATE USER 'djangoi_app'@'localhost' IDENTIFIED BY 'django_app';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON stepic_webdev.* TO 'django_app'@'localhost';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS stepic CHARACTER SET utf8;"
+mysql -u root -e "CREATE USER 'django'@'localhost' IDENTIFIED BY 'django';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON stepic.* TO 'django'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
