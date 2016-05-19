8d85c66b56aefe705c7b961fc30be6f6b8a0f2d3
Andrey Shibalov <nekorro@gmail.com>
2016-05-19 10:56:08 +0300

    fixed db integration

--- init.sh
+++ init.sh
@@ -4,13 +4,3 @@
 sudo apt-get update nginx
 sudo pip install --upgrade django
 sudo pip install --upgrade gunicorn
-
-# configure nginx
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/ash.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-
-# create db
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS stepic-webdev CHARACTER SET utf8;"
-mysql -uroot -e "CREATE USER 'django' IDENTIFIED BY 'django';"
-mysql -uroot -e "GRANT ALL ON stepic-webdev.* TO 'django';"
